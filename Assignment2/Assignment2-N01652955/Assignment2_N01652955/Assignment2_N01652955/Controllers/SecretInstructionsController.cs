e089461e2279e4c26300e9ae3f2ef262a7b0350f
pranjall <pranjalmlokhande@gmail.com>
2024-06-28 01:57:18 -0400

    pushed 28.6

---
+++ Assignment2/Assignment2-N01652955/Assignment2_N01652955/Assignment2_N01652955/Controllers/SecretInstructionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2_N01652955.Controllers
+{
+    public class SecretInstructionsController : ApiController
+    {
+        [HttpGet]
+        [Route("/api/J3/Codes")]
+        public string instructionCodes()
+        {
+            int instructionCode = "57234\r\n00907\r\n34100\r\n99999";
+            return instructionCode;
+        }
+
+    }
+}
